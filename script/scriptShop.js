bc3deb55a8739e2b9bd8c1d8ecc71b40313dc4d9
RusuSebastian <andreisebastian.rusu1997@gmail.com>
2021-08-27 02:57:46 +0300

    ads

--- script/scriptShop.js
+++ script/scriptShop.js
@@ -16,7 +16,7 @@ if(darkMode==="enabled"){
 chk.addEventListener('change', () => {
 	darkMode=localStorage.getItem('darkMode');
   toggleDarkMode();
-  console.log(darkMode);
+  
 });
 //DARKMODE END
 
@@ -24,7 +24,6 @@ chk.addEventListener('change', () => {
 //cart pop-up
 const cartModal=document.getElementsByClassName("widget-cart")[0];
 const myCartBtn=document.getElementsByClassName("my-cart")[0];
-console.log(cartModal,myCartBtn);
 myCartBtn.addEventListener("click", ()=>{
   cartModal.classList.toggle("show");
   
@@ -120,111 +119,10 @@ function windowClicked (buttonClass,dropdown){
 
 
 //SHOW ITEMS
-
-  const items = [
-    {
-      type: ["Machines","Cup and Glass","Health & Safety","Varieties","Excels Coffee"],
-      url: "./photos/items/coffee2.jpg",
-      url2: "./photos/items/coffee3.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "32"
-    },
-    {
-      type: ["Coffee Bean","Machines","Cup and Glass","Health & Safety","Varieties","Excels Coffee"],
-      url: "./photos/items/coffee2.jpg",
-      url2: "./photos/items/coffee3.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "10.00",
-      discount: "43"
-    },
-    {
-      type: ["Coffee Bean","Machines","Cup and Glass","Health & Safety","Varieties","Excels Coffee"],
-      url: "./photos/items/coffee2.jpg",
-      url2: "./photos/items/coffee3.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "12"
-    },
-    {
-      type:["Coffee Bean","Machines","Cup and Glass","Health & Safety","Varieties","Excels Coffee"],
-      url: "./photos/items/coffee2.jpg",
-      url2: "./photos/items/coffee3.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "0"
-    },
-    {
-      type: "Machines",
-      url: "./photos/items/coffee1.jpg",
-      url2: "./photos/items/coffee4.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "32"
-    },
-    {
-      type: "Coffee Bean",
-      url: "./photos/items/coffee3.jpg",
-      url2: "./photos/items/coffee4.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "54"
-    },
-    {
-      type: "Coffee Bean",
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    },
-    {
-      type: ["Health & Safety","Varieties","Coffee Bean"],
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    },
-    {
-      type: ["Health & Safety","Varieties"],
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    },
-    {
-      type: ["Health & Safety","Varieties"],
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    },
-    {
-      type: ["Health & Safety","Varieties"],
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    },
-    {
-      type: ["Health & Safety","Varieties"],
-      url: "./photos/items/coffee4.jpg",
-      url2: "./photos/items/coffee5.jpg",
-      info: "Sky Blue Designer Pink Longue Tub",
-      price: "100.00",
-      discount: "35"
-    }
-  ];
-
-  const categItms = document.querySelector(".bot-side");
-
-function showItems() {
+const categItms = document.querySelector(".bot-side");
+function showItems(categ) {
   for (const item of allItems) {
-      
+    if(item.categories==categ){  
       const product = document.createElement("div");
       product.classList.add("product-item");
       product.onmouseover = function () {
@@ -337,7 +235,6 @@ function showItems() {
         modalContainer.appendChild(rightSide);       
    });
 
-
       const viewImg=document.createElement("img");
       viewImg.src="./photos/item-hover/eye.svg";
       view.appendChild(viewImg);
@@ -350,11 +247,186 @@ function showItems() {
       wishImg.src="./photos/item-hover/heart.svg";
       wish.appendChild(wishImg);
     }
+  }
 };
-showItems();
+
+
+
+
+
+let row=12;
+let current_page=1;
+function createElements(items,items_per_page, page){ 
+  categItms.innerHTML="";
+  page--;
+  let start=items_per_page * page;
+  let end = start + items_per_page;
+  let paginatedallItems=items.slice(start,end);
+  
+      for(const item of paginatedallItems){
+      const product = document.createElement("div");
+      product.classList.add("product-item");
+      product.onmouseover = function () {
+      imgProduct.src = item.photos[1];
+
+      };
+      product.onmouseout = function () {
+        imgProduct.src = item.photos[0];
+      };
+      categItms.appendChild(product);
+      const imgProduct = document.createElement("img");
+      imgProduct.setAttribute("id", "my-img");
+      imgProduct.src = item.photos[0];
+      product.appendChild(imgProduct);
+      const productInfo = document.createElement("div");
+      productInfo.classList.add("product-info");
+      product.appendChild(productInfo);
+      const pInfo = document.createElement("a");
+      pInfo.classList.add("info");
+      pInfo.href="item-page.html";
+      
+      pInfo.onclick=()=>{
+        localStorage.setItem("id",item.id);
+        localStorage.setItem("categories",item.categories);
+
+      }
+
+      pInfo.innerText = item["name"];
+      productInfo.appendChild(pInfo);
+
+      const pminiDrisc = document.createElement("div");
+      pminiDrisc.classList.add("mini-drisc");
+      productInfo.appendChild(pminiDrisc);
+      const info=document.createElement("p");
+      info.classList.add("mini-discript")
+      info.innerText=item.smallDescription;
+      info.style.fontSize="12px";
+      info.style.marginTop="10px";
+      info.style.color="#838383";
+      info.style.display="none";
+      pminiDrisc.appendChild(info);
+
+      const pPrice = document.createElement("p");
+      pPrice.classList.add("price");
+      pPrice.innerText = `$${item.price}`;
+      productInfo.appendChild(pPrice);
+
+      //
+      const oldPrice = document.createElement("span");
+      oldPrice.classList.add("old-price");
+      function old() {
+        if (item.discount > 0) {
+          oldPrice.innerText = ` $ ` + ((item.discount / 100) * item.price + Number(item.price)).toFixed(2);
+        }
+      };
+      old();
+      pPrice.appendChild(oldPrice);
+      //
+      const discPrice = document.createElement("span");
+      discPrice.classList.add("discount");
+      function disc() {
+        if (item.discount > 0) {
+          discPrice.innerText = `-${item.discount}%`
+        }
+      };
+      disc();
+      pPrice.appendChild(discPrice);
+      //
+      const productBtns = document.createElement("div");
+      productBtns.classList.add("product-btns");
+      productInfo.appendChild(productBtns);
+      const cart = document.createElement("div");
+      cart.classList.add("cart");
+      productBtns.appendChild(cart);
+
+      const cartImg=document.createElement("img");
+      cartImg.src="./photos/item-hover/shopping-basket.svg";
+      cart.appendChild(cartImg);
+
+      const view = document.createElement("button");
+      view.classList.add("view-modal");
+      productBtns.appendChild(view);
+
+      view.addEventListener("click",()=>{       
+        modal.classList.add("show-modal"); 
+        const modalContainer=document.querySelector(".modal-content");
+        const leftSide=document.querySelector(".left-side");
+        const rightSide=document.querySelector(".right-side");
+        leftSide.innerHTML=`<img src="${item.photos[0]}" alt="" > `;
+        rightSide.innerHTML=`
+        <h2 class="title-item">${item.name}</h2>
+        <p class="price">$${item.price} <span class="price-discount">${item.discount>0?'$'+((item.discount / 100) * item.price + Number(item.price)).toFixed(2):``}</span></p>
+        <p class="small-description">${item.smallDescription}</p>
+        <p class="stock"><span class="stock-span">${item.stock} </span>in stock</p>
+        <form action="" class="Qty-add-cart">
+            <label for="qty">Qty:</label>
+            <input type="number" name="qty" id="qty" min="1" step="1" max="95"
+            value="1" inputmode="numeric" pattern="[0-9]" >
+            <button type="submit">ADD TO CART</button>
+        </form>
+        <p class="sku">SKU:<span class="sku-span"> NHFL5</span></p>
+        <p class="categories-item">Categories:<span class="categories-item-span">${item.categories[0]}</span></p>
+        <button class="close-modal">Close</button>`;       
+         const closeModal=document.querySelector(".close-modal");
+         closeModal.addEventListener("click", ()=>{
+         modal.classList.remove("show-modal");
+         });
+        modalContainer.appendChild(leftSide);       
+        modalContainer.appendChild(rightSide);       
+   });
+      const viewImg=document.createElement("img");
+      viewImg.src="./photos/item-hover/eye.svg";
+      view.appendChild(viewImg);
+
+      const wish = document.createElement("button");
+      wish.classList.add("wishlist");
+      productBtns.appendChild(wish);
+
+      const wishImg=document.createElement("img");
+      wishImg.src="./photos/item-hover/heart.svg";
+      wish.appendChild(wishImg);
+      
+    }
+};
+createElements(allItems,row,current_page);
+
+
+function paginationBtns(items){
+    const btnsPag=document.querySelector(".pagination");
+    btnsPag.innerHTML="";
+    let page_count=Math.ceil(items.length/row);
+    for(i=1;i<page_count+1;i++){
+      let btn=document.createElement('button');
+      btn.innerText=i;
+      btnsPag.appendChild(btn);
+    }
+    btnsPag.firstElementChild.classList.add("current-pag");
+
+    btnsPag.addEventListener("click",(e)=>{
+    [...btnsPag.children].forEach(element =>{
+      element.classList.remove("current-pag");
+    });
+    e.target.classList.add("current-pag");
+    current_page=Number(e.target.innerText);
+    categItms.innerHTML="";
+
+    createElements(items,row,current_page);
+    });
+    
+};
+paginationBtns(allItems);
+
+
+
+
+
+
+
+
+
+
+
 //SHOW ITEMS END
-
-
 
 
 
@@ -363,8 +435,9 @@ const btnList=document.querySelector(".list");
 btnList.addEventListener("click",()=>{
   
   const content=document.querySelector(".bot-side");
-  content.innerHTML="";
-  showItems();
+  // content.innerHTML="";
+  // showItems();
+  // createElements(allItems,row,current_page);
   
   btnList.classList.add("activ-shop-view");
   btnGrid.classList.remove("activ-shop-view");
@@ -388,14 +461,26 @@ btnList.addEventListener("click",()=>{
 });
 
 const btnGrid=document.querySelector(".grid");
+
 btnGrid.addEventListener("click",()=>{
   btnList.classList.remove("activ-shop-view");
   btnGrid.classList.add("activ-shop-view");
   const content=document.querySelector(".bot-side");
-  content.innerHTML="";
+  // content.innerHTML="";
   content.style.display="grid";
-  showItems();
-  
+  // showItems();
+  // createElements(allItems,row,current_page);
+  const items=[...content.children];
+  for(let item of items){
+    item.style.flexDirection="column";
+    item.firstElementChild.style.width="auto";
+    item.lastElementChild.style.overflow="hidden";
+    item.lastElementChild.style.padding="0px"; 
+  }
+  const allminidiscript=[...document.querySelectorAll(".mini-discript")];
+  for (let mini of allminidiscript){
+    mini.style.display="none";
+  }
 });
 //VIEW LIST TYPE END
 
@@ -408,3 +493,52 @@ closeModal.addEventListener("click", ()=>{
   modal.classList.remove("show-modal");
 });
 //MODAL END
+
+//categories
+const dropdownContainer=document.querySelector(".dropdown");
+dropdownContainer.addEventListener("click",(e)=>{
+categItms.innerHTML="";
+let newArray=[];
+if(e.target.innerText=="All Categories"){
+  
+  createElements(allItems,row,current_page);
+  filter(allItems);
+  paginationBtns(allItems);
+}else{
+
+for(const element of allItems){
+ if(element.categories == e.target.innerText){
+  newArray.push(element);
+ } 
+};
+showItems(e.target.innerText);
+filter(newArray);
+paginationBtns(newArray);
+}
+
+
+});
+
+//
+function filter(arraytouse){
+const selectFilter=document.querySelector("#sorting");
+selectFilter.addEventListener("change",(e)=>{
+const [key, direction] = e.target.value.split("-");
+array=arraytouse;
+array.sort((a, b) => {
+  if (direction == "asc") {
+    if (Number(a[key]) < Number(b[key])) return -1;
+    if (Number(a[key]) > Number(b[key])) return 1;
+    return 0;
+  }
+  if (Number(a[key]) < Number(b[key])) return 1;
+  if (Number(a[key]) > Number(b[key])) return -1;
+  return 0;
+  
+});
+categItms.innerHTML="";
+createElements(array,row,current_page);
+paginationBtns(array);
+});
+}
+filter(allItems)
